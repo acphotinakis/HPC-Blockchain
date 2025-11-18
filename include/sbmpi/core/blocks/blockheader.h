ca7a00dae6a02807e55942e14540e2a9a8ad70de
Andrew Photinakis <andrewcphotinakis@gmail.com>
2025-11-17 19:18:09 -0500

    updating

--- include/sbmpi/core/blocks/blockheader.h
+++ include/sbmpi/core/blocks/blockheader.h
@@ -5,27 +5,31 @@
 #include <string>
 #include <vector>
 
-namespace sbmpi {
-namespace core {
-namespace blocks {
+namespace sbmpi
+{
+  namespace core
+  {
+    namespace blocks
+    {
 
-class BlockHeader {
- public:
-  int height;
-  std::string previousHash;
-  std::string merkleRoot;
-  std::chrono::system_clock::time_point timestamp;
+      class BlockHeader
+      {
+       public:
+        int                                   height;
+        std::string                           previousHash;
+        std::string                           merkleRoot;
+        std::chrono::system_clock::time_point timestamp;
 
-  BlockHeader();
-  BlockHeader(int height, const std::string& previousHash,
-              const std::string& merkleRoot);
-  std::string hash() const;
-  std::vector<char> serialize() const;
-  void deserialize(const std::vector<char>& data);
-};
+        BlockHeader();
+        BlockHeader(int height, const std::string& previousHash,
+                    const std::string& merkleRoot);
+        std::string       hash() const;
+        std::vector<char> serialize() const;
+        void              deserialize(const std::vector<char>& data);
+      };
 
-}  // namespace blocks
-}  // namespace core
+    }  // namespace blocks
+  }  // namespace core
 }  // namespace sbmpi
 
 #endif  // SBMPI_BLOCKHEADER_H
--- include/sbmpi/core/blocks/macro_block.h
+++ include/sbmpi/core/blocks/macro_block.h
@@ -5,23 +5,27 @@
 #include "block.h"
 #include "micro_block.h"
 
-namespace sbmpi {
-namespace core {
-namespace blocks {
+namespace sbmpi
+{
+  namespace core
+  {
+    namespace blocks
+    {
 
-class MacroBlock : public Block {
- public:
-  std::vector<std::string> microBlockHashes;
+      class MacroBlock : public Block
+      {
+       public:
+        std::vector<std::string> microBlockHashes;
 
-  MacroBlock();
-  std::string getType() const override;
-  void addMicroBlock(const MicroBlock& microBlock);
-  std::vector<char> serialize() const override;
-  void deserialize(const std::vector<char>& data) override;
-};
+        MacroBlock();
+        std::string       getType() const override;
+        void              addMicroBlock(const MicroBlock& microBlock);
+        std::vector<char> serialize() const override;
+        void              deserialize(const std::vector<char>& data) override;
+      };
 
-}  // namespace blocks
-}  // namespace core
+    }  // namespace blocks
+  }  // namespace core
 }  // namespace sbmpi
 
 #endif  // SBMPI_MACRO_BLOCK_H
